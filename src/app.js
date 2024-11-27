b93c1a7fcd9b46008826e1983db5f44bdaa78d97
Mohd Saad <shaadansari8081@gmail.com>
2024-11-27 20:57:48 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -1,39 +1,28 @@
-const express=require('express');
+const express = require("express");
 
-const app=express();
+const app = express();
 // console.log(app);
 
 
-app.use("/Home",(req,res)=>{
-    res.send('Hello Worlds home')
+
+
+
+app.use("/hello/2", (req, res) => {
+  res.send("Abra ka dabra from hello2 ");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello sello bye from hello ");
 });
 
+app.use("/test", (req, res) => {
+  res.send("Hello from the server from test");
+});
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello sello bye ")
-    
-})
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
-    
-})
+// app.use("/", (req, res) => {
+//   res.send("Hello Worlds home from /");
+// });
 
-app.listen(7777,()=>{
-    console.log("server is sucessfully listening");
-    
-})
+app.listen(7777, () => {
+  console.log("server is sucessfully listening");
+});
 
-
-// const express=require('express');
-
-// const app=express();
-
-// app.use((req,res)=>{
-//     res.send("Hello from the server saad")
-// })
-
-
-// app.listen(4000,()=>{
-//     console.log("From the server");
-    
-// })
