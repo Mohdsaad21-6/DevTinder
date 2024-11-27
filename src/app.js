3e908d75e838339a632d6cc2266f0b1bd349fcb2
Mohd Saad <shaadansari8081@gmail.com>
2024-11-27 23:58:36 +0530

    27

--- src/app.js
+++ src/app.js
@@ -2,30 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user",(req,res)=>{
-    res.send("hahahhahah")
-})
-
-//This will match only GET call to /user
-
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Mohd ",lastName:"Saad"});
-})
-
-app.post("/user",(req,res)=>{
-    // console.log("Save dataa to the DB");
-    //Saving data to DB
-    res.send("Data successfully save to the DB")
+//ac  //abc
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
     
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfuly")
-})
-//This will match all the http methods API call to/test
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server from test");
+  res.send({ firstname: "Mohd ", lastName: "Saad" });
 });
 
 app.listen(7777, () => {
