cf7f4605b5ff5120a411b5e1d3ddf7fb3475dda7
Mohd Saad <shaadansari8081@gmail.com>
2024-11-28 20:50:07 +0530

    next

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
-//ac  //abc
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    
-  res.send({ firstname: "Mohd ", lastName: "Saad" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    //route handler 1
+    // res.send("Route Handler 1")
+    console.log("Handling the route user!!");
+    // res.send("Response");
+    next();
+  },
+  //route handler 2
+  (req, res,next) => {
+    console.log("Handling the route user2!!");
+    res.send("Response 2nd");
+    next();  
+  },
+  //route handler 3
+  (req, res) => {
+    console.log("Handling the route user3!!");
+    res.send("Response 3nd");
+  }
+);
 
 app.listen(7777, () => {
   console.log("server is sucessfully listening");
