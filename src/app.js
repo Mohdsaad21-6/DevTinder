2658cb0db1e8dfc94873bccb3f5e0256679e6847
Mohd Saad <shaadansari8081@gmail.com>
2024-12-02 15:11:00 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -1,28 +1,22 @@
 const express = require("express");
 
+const { adminAuth ,userAuth} = require("./middlewares/auth");
+
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    //route handler 1
-    // res.send("Route Handler 1")
-    console.log("Handling the route user!!");
-    // res.send("Response");
-    next();
-  },
-  //route handler 2
-  (req, res,next) => {
-    console.log("Handling the route user2!!");
-    res.send("Response 2nd");
-    next();  
-  },
-  //route handler 3
-  (req, res) => {
-    console.log("Handling the route user3!!");
-    res.send("Response 3nd");
-  }
-);
+//handle auth middleware for all request get, post ,delete ,put
+app.use("/admin", adminAuth);
+
+app.use("/user", (req, res) => {
+  res.send("user data");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 
 app.listen(7777, () => {
   console.log("server is sucessfully listening");
