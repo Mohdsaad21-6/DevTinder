17611c4e7aeb2a4eb1f9177ec357f9e60c790cd1
Mohd Saad <shaadansari8081@gmail.com>
2024-12-06 15:40:52 +0530

    schemas  E6

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.get("/getUserData", (req, res) => {
-  throw new Error("asdfghjk");
-  //logic of db call and get user data
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Fahad",
+    lastName: "Ansari",
+    emailId: "Fahad@gmail.com",
+    password: "8887927092",
+    _id: "80819999999",
+  });
 
-  res.send("user data");
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    //log the errors
-    res.sendStatus(500);
-    // .send("something went wronged")
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.sendStatus("404");
   }
 });
-
-app.listen(7777, () => {
-  console.log("server is sucessfully listening");
-});
+ 
+connectDB()
+  .then(() => {
+    console.log("database connection is succesfully established");
+    app.listen(7777, () => {
+      console.log("server is sucessfully listening");
+    });
+  })
+  .catch((err) => {
+    console.error("database cant connected");
+  });
