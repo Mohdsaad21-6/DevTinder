28452c6be6a6a13908c1119f105100c9d656faeb
Mohd Saad <shaadansari8081@gmail.com>
2025-04-07 19:14:21 +0530

    Refactor app.js to improve code readability and structure; ensure cron job is loaded after database connection

--- src/app.js
+++ src/app.js
@@ -2,18 +2,16 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
-const cors=require("cors")
+const cors = require("cors");
 
+require("dotenv").config();
 
-require("dotenv").config()
-
-
-require("./utils/cronjob")
-
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,17 +19,19 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-const paymentRouter=require("./routes/payment")
+const paymentRouter = require("./routes/payment");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-app.use("/",paymentRouter)
+app.use("/", paymentRouter);
 
 connectDB()
   .then(() => {
     console.log("Database connection is successfully established");
+    require("./utils/cronjob");
+
     app.listen(process.env.PORT, () => {
       console.log("Server is successfully listening on port 7777");
     });
